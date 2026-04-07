5a223c19638b13079452253b67cfa27459941cfc
unknown <akshaykrish533516@gmail.com>
2026-04-07 12:56:03 +0530

    Implemented profile viewing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = ({showLogin=true,showSignup=true,rightSlot,leftSlot,title='SkillShare'}) => {
   return (
     <nav className='bg-amber-300 flex p-4 justify-between items-center '>
-      <h1 className='font-bold text-2xl'>{title}</h1>
+      <h1 className='font-bold text-2xl'><Link to='/'>{title}</Link></h1>
       <div className='flex items-center gap-7'>
         {showLogin &&(
           <Link to='/login' className='hover:font-bold cursor-pointer'>Login</Link>
@@ -13,6 +13,7 @@ const Navbar = ({showLogin=true,showSignup=true,rightSlot,leftSlot,title='SkillS
         {showSignup && (
           <button className='bg-amber-600 p-2 hover:bg-amber-800 cursor-pointer text-white rounded'><Link to='/register'>Sign Up for free</Link></button>
         )}
+        
         {rightSlot}
       </div>
     </nav>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Navbar from '../components/Navbar.jsx'
 import { AuthContext } from '../context/AuthContext.jsx'
+import { Link } from 'react-router-dom'
 
 const Profile = () => {
 
@@ -33,9 +34,14 @@ const Profile = () => {
       <button className='bg-amber-950 text-white p-2 rounded hover:bg-amber-900 cursor-pointer'onClick={logout}>Logout</button>
     }
     />
-    <div>{profile.name}</div>
-    <div>{profile.email}</div>
-    <div>{profile.bio}</div>
+    <div className='mt-4 px-2'><h1 className='text-3xl font-extrabold'>Profile Information:</h1></div>
+    <div className='flex mt-4 px-2 gap-2 text-2xl'><p className='font-bold'>Name:</p>{profile.name}</div>
+    <div className='flex mt-4 px-2 gap-2 text-2xl'><p className='font-bold'>Email:</p>{profile.email}</div>
+    <div className='flex mt-4 px-2 gap-2 text-2xl'><p className='font-bold'>About me:</p>{profile.bio}</div>
+    <div className='flex mt-8 px-2 gap-3'>
+      <Link to='/dashboard' className='text-white bg-black p-2'><p>Go back to dashboard</p></Link>
+      <button className='bg-purple-700 p-2 text-white'>Edit Profile</button>
+    </div>
     </>
   )
 }
